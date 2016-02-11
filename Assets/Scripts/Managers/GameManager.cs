315466c76da9c074eb1ac28cc6548d451b722002
alexscarlatos <alexscarlatos@Alexs-MacBook-Pro.local>
2016-02-11 17:17:01 -0500

    Made Base Classes

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameState state;
+	public static GUIManager gui;
+	public static SoundManager sound;
+	public static Factory factory;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
