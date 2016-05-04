aa835ad5c2579615c97c19ca4c1c43186414ffca
Marlene Shankar <marleneshankar@Marlenes-MacBook-Pro.local>
2016-05-04 12:14:53 -0400

    Took out errors in Reaction Class to work on

--- Assets/Scripts/Objects/Reaction.cs
+++ Assets/Scripts/Objects/Reaction.cs
@@ -4,20 +4,23 @@ using System.Collections;
 // Keeps track of the reactions in the game
 public class Reaction : MonoBehaviour {
 
-	public ArrayList reactants;
+	public ArrayList reaction;
 	public int playerReactant;
 	public int gameMolecule;
 	// Treat reaction as a stack for undoing functionality
+	// Store reactants and then products
 	// Map reactants to products using indicies
 	// Have 2D array and look at x and y to get product
 	// playerReactant = x
 	// gameMolecule = y
 
 
-	//public Molecule returnProduct(int playerReactant, int gameMolecule){
+	public void returnProduct(Molecule playerReactant, Molecule gameMolecule, Molecule [,] reactionTable){
 		// go to the index of the two ints to get the product
-		// return product
 
-	//}
+		// return product
+		//Molecule product = new Molecule();
+
+	}
 		
 }
