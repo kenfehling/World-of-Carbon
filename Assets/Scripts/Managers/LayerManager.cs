ad206a8b93a1f618c223a64dc3a7ab32b146c1e2
Alec Day <shirojyuu@gmail.com>
2016-06-18 15:33:21 -0400

    Added in all the mantle pieces for the second layer.

---
+++ Assets/Scripts/Managers/LayerManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArtManager : MonoBehaviour {
+
+    public Texture2D[] layers = new Texture2D[3];
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
