5b84daae8c661b9c1bb7aaaa3179a16e578a2e57
Alec Day <shirojyuu@gmail.com>
2016-06-18 17:39:05 -0400

    Fixed Particle Effect to be in only X-Y axes; added lerp to fade the layers in.

---
+++ Assets/Resources/Misc/FaderScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaderScript : MonoBehaviour {
+    SpriteRenderer rend;
+    Color tar;
+    Color cur;
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rend.color = Color.Lerp(rend.color, Color.white, Time.deltaTime * 4.0f);
+	}
+
+    void OnEnable()
+    {
+        Debug.Log("Lerp In");
+        rend.color = Color.clear;
+        tar = Color.white;
+        Debug.Log(transform.localScale);
+        
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("Lerp Out");
+        rend.color = Color.white;
+        tar = Color.clear;
+
+    }
+}
