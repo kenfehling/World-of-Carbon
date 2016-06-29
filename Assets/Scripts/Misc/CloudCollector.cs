c26e360ef28a344a1c43dfe349c60c20b888988d
Alec Day <shirojyuu@gmail.com>
2016-06-28 21:16:49 -0400

    Added in Carbon Cloud with dynamic carbon emission

---
+++ Assets/Scripts/Misc/CloudCollector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudCollector : MonoBehaviour {
+
+    private int baseNumber = 100;
+    public int numberOfCarbons = 0;
+    public int cloudFactor = 20;
+    private ParticleSystem cloud;
+	// Use this for initialization
+	void Start () {
+        cloud = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cloud.maxParticles = baseNumber + (numberOfCarbons * cloudFactor);
+	}
+}
