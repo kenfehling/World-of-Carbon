ae9333a83f7369b159d0eda60b4ce90de650b1bd
Michael Witkovsky <michael.witkovsky@gmail.com>
2016-06-28 20:58:48 -0400

    yeah the carbides now explode into multiple carbons or whatever

--- Assets/Scripts/Entities/Carbide.cs
+++ Assets/Scripts/Entities/Carbide.cs
@@ -3,8 +3,51 @@ using System.Collections;
 
 public class Carbide : MonoBehaviour {
 
+    public float delay = 0.1f;
+    public int carbonYield = 5;
+
     private GameObject product;
     private PlayerManager player;
+    private float timer;
+    private bool exploding;
+
+    void Start()
+    {
+        timer = delay;
+    }
+
+    void FixedUpdate()
+    {
+        if (exploding)
+        {
+            timer -= Time.fixedDeltaTime;
+            if (timer < 0.0f)
+            {
+                timer = delay;
+                --carbonYield;
+
+                //Spawn the explosion particle effect
+                Instantiate(Resources.Load(ResourcePaths.transExplosion), transform.position, Quaternion.identity);
+
+                //Spawn a lone carbon and send it flying to the player
+                product = (GameObject)Instantiate(Resources.Load(ResourcePaths.FreeCarbonMolecule), transform.position, Quaternion.identity);
+                product.GetComponent<FlyTo>().setTarget(player.transform);
+
+                //Spawn other products and send them flying in a random direction
+
+                //Play combine sound
+                AudioSource aSrc = player.GetAudioSource();
+                SoundManager soundManager = GameObject.Find("GameManager").GetComponent<GameManager>().getSoundManager();
+                soundManager.playCombineSound(aSrc);
+
+                if (carbonYield == 0)
+                {
+                    //Destroy the carbide
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -25,23 +68,7 @@ public class Carbide : MonoBehaviour {
 
             if (player.IsOxidePresent())
             {
-                //Spawn the explosion particle effect
-                Instantiate(Resources.Load(ResourcePaths.transExplosion), transform.position, Quaternion.identity);
-
-                //Spawn a lone carbon and send it flying to the player
-                product = (GameObject)Instantiate(Resources.Load(ResourcePaths.FreeCarbonMolecule), transform.position, Quaternion.identity);
-                product.GetComponent<FlyTo>().setTarget(player.transform);
-
-                //Spawn other products and send them flying in a random direction
-
-
-                //Play combine sound
-                AudioSource aSrc = player.GetAudioSource();
-                SoundManager soundManager = GameObject.Find("GameManager").GetComponent<GameManager>().getSoundManager();
-                soundManager.playCombineSound(aSrc);
-
-                //Destroy the carbide
-                Destroy(gameObject);
+                exploding = true;
             }   
         }
     }
