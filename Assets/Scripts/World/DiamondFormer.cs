e5f8757f853a6eec04ebe7c424ccbb3157767f85
Michael Witkovsky <michael.witkovsky@gmail.com>
2016-06-28 21:15:11 -0400

    Diamond former

---
+++ Assets/Scripts/World/DiamondFormer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiamondFormer : MonoBehaviour {
+
+    public int minimumCarbon = 30;
+
+    private PlayerManager player;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        OnTriggerStay2D(other);
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerManager>())
+        {
+            if (!player)
+            {
+                player = other.GetComponent<PlayerManager>();
+            }
+
+            if(player.GetNumOfCarbons() > minimumCarbon)
+            {
+                //YOU WIN
+            }
+        }
+    }
+}
