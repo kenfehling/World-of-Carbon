8f1885a77dc1272714e31f2cb04ccce8931dfc77
Alec Day <shirojyuu@gmail.com>
2016-06-30 18:33:50 -0400

    New background, new floaty particles.

---
+++ Assets/Scripts/Misc/RandParticles.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandParticles : MonoBehaviour {
+
+	// Use this for initialization
+    public Material[] particleSprites = new Material[5];
+	void Start () {
+        ParticleSystemRenderer psr = GetComponent<ParticleSystem>().GetComponent<ParticleSystemRenderer>();
+        psr.material = particleSprites[Random.Range(0, 4)];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
