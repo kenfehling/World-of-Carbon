94256023992603e94d603120ff573afba2467b5d
Alec Day <shirojyuu@gmail.com>
2016-06-30 18:57:11 -0400

    Added in functionality for Ejector script; you can be booted out with the wrong number of carbon. Ready for deliverable 2.

--- Assets/Scripts/World/Ejector.cs
+++ Assets/Scripts/World/Ejector.cs
@@ -4,18 +4,27 @@ using System.Collections;
 public class Ejector : MonoBehaviour {
 
     public int minimumCarbons;
-
+    private bool ejected = false;
     private PlayerManager player;
 
+    void OnEnable()
+    {
+        ejected = false;
+    }
 	void FixedUpdate () {
         if (!player)
         {
-            GameObject.Find("Player");
+            player = GameObject.Find("Player").GetComponent<PlayerManager>();
         }
 
         if(player.GetNumOfCarbons() < minimumCarbons)
         {
-            GameObject.Find("GameManager").gameObject.GetComponent<LayerManager>().SwitchLayer(GameObject.Find("GameManager").gameObject.GetComponent<LayerManager>().GetCurrentLayer() - 1);
+            if (!ejected)
+            {
+                GameObject.Find("GameManager").gameObject.GetComponent<LayerManager>().SwitchLayer(GameObject.Find("GameManager").gameObject.GetComponent<LayerManager>().GetCurrentLayer() - 1);
+                ejected = true;
+                GameObject.Find("GameManager").gameObject.GetComponent<GameManager>().getMusicManager().moveUpLayer();
+            }
         }
 	}
 }
