fb90d458f31ded3157ddea5650eb00e643b6b49b
Michael Witkovsky <Michael.Witkovsky@gmail.com>
2016-06-30 13:58:37 -0400

    Added UI and carbon HUD

---
+++ Assets/Scripts/UI/CarbonDisplay.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CarbonDisplay : MonoBehaviour {
+
+    private Text text;
+    private string displayText;
+    private int numOfCarbons;
+
+	// Use this for initialization
+	void Start () {
+        text = gameObject.GetComponent<Text>();
+
+        displayText = "Number of Carbons: ";
+        numOfCarbons = 1;
+        updateDisplay();
+	}
+
+    private void updateDisplay()
+    {
+        text.text = displayText + numOfCarbons;
+    }
+	
+	public void setNumOfCarbons(int numOfCarbons)
+    {
+        this.numOfCarbons = numOfCarbons;
+        updateDisplay();
+    }
+}
