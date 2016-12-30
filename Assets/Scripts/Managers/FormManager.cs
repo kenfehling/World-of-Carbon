06b16cf9ec4d9f0c432bb9b7a0d8e4c1e032b6d9
Alec Day <shirojyuu@gmail.com>
2016-12-30 17:32:11 -0500

    Made the FormManager work more consistently

--- Assets/Scripts/Managers/FormManager.cs
+++ Assets/Scripts/Managers/FormManager.cs
@@ -22,28 +22,34 @@ public class FormManager : MonoBehaviour {
 	void Update () {
         currentForm = pMol.formula;
 
-        Match m;
-        if((m = Regex.Match(currentForm, "^C$")).Success)
+        if(currentForm == "CaCO3" || currentForm == "MantleCaCO3")
+        {
+            currentForm = "CO3";
+        }
+
+        currentForm = currentForm.Substring(currentForm.IndexOf("C"));
+        Debug.Log(currentForm);
+        if(currentForm == "C")
         {
             Debug.Log("You are C");
             moleculeSprite.sprite = carbonForms[0];
         }
 
-        else if ((m = Regex.Match(currentForm, "^CO2$")).Success)
+        else if (currentForm == "CO2")
         {
             Debug.Log("You are CO2");
             moleculeSprite.sprite = carbonForms[1];
 
         }
 
-        else if ((m = Regex.Match(currentForm, "^CO3$")).Success)
+        else if (currentForm == "CO3")
         {
             Debug.Log("You are CO3");
             moleculeSprite.sprite = carbonForms[2];
 
         }
 
-        else if ((m = Regex.Match(currentForm, "^CDiamond$")).Success)
+        else if (currentForm.IndexOf("CDiamond") != -1)
         {
             Debug.Log("You are Diamond!");
             moleculeSprite.sprite = carbonForms[3];
